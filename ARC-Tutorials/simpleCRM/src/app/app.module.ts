2dead967c8858100261f8e28deabf53805f9f05f
KESARI Abhinav <abhinav.kesari@soprasteria.com>
2022-12-02 17:40:12 +0530

    login component added

--- ARC-Tutorials/simpleCRM/src/app/app.module.ts
+++ ARC-Tutorials/simpleCRM/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

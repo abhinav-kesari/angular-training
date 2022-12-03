2789764c1052d29191f7b4bc75cd5b78cccc7abf
KESARI Abhinav <abhinav.kesari@soprasteria.com>
2022-12-03 18:53:30 +0530

    Lifecycle hooks

--- ARC-Tutorials/simpleCRM/src/app/contacts/add-contact/add-contact.component.ts
+++ ARC-Tutorials/simpleCRM/src/app/contacts/add-contact/add-contact.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
   styleUrls: ['./add-contact.component.scss']
 })
-export class AddContactComponent implements OnInit {
+export class AddContactComponent implements OnInit , OnChanges ,OnDestroy{
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(): void {
+    
+  }
+  ngOnDestroy(): void {
+    
+  }
 }

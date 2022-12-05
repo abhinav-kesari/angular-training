5cc750f5993416b8a1fa0b7cf16adedb9cca0006
KESARI Abhinav <abhinav.kesari@soprasteria.com>
2022-12-05 17:13:15 +0530

    ngFor with index , first, last,even, odd

--- ARC-Tutorials/simpleCRM/src/app/app.component.ts
+++ ARC-Tutorials/simpleCRM/src/app/app.component.ts
@@ -9,4 +9,21 @@ export class AppComponent {
   title = 'simpleCRM';
   show_success = false ;
   show_success_or = true ;
+  contacts =[
+    {
+      "firstName" : "Abhinav",
+      "lastName"  : "Kesari",
+      "id"        : 123
+    },
+    {
+      "firstName" : "Ankur",
+      "lastName"  : "Kesari",
+      "id"        : 345
+    },
+    {
+      "firstName" : "Shiva",
+      "lastName"  : "Shiva",
+      "id"        : 786
+    }
+  ]
 }

40853cbe2885431cbd8b087f1d36164a40300073
KESARI Abhinav <abhinav.kesari@soprasteria.com>
2023-03-23 23:36:01 +0530

    8

--- ARC-Tutorials/basic-of-angular/src/app/app.component.ts
+++ ARC-Tutorials/basic-of-angular/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+
+  ngOnInit(): void {
+   console.log()
+  }
+
   title = 'basic-of-angular';
+  userLoggedIn: boolean | undefined;
+  myObservable  = of (1, 2, 3);
+  obs= new Observable((observer) => {
+  console.log("Observable starts");  
+  setTimeout(()=> { observer.next("2") }, 2000);
+  setTimeout(()=> { observer.next("2") }, 2000);
+  setTimeout(() => { observer.next("3") }, 3000);
+  setTimeout(() => { observer.next("4") }, 4000);
+  setTimeout(() => { observer.next("5") }, 5000);
+  console.log("Observable ends");
+});
 }
